3dadc3f58f6c9f6da9b91fff5ee740d0fa184fac
hud <baniczek@gmail.com>
2017-08-31 08:54:09 +0200

    working 1

--- src/Impulse/NeuralNetwork/Network.h
+++ src/Impulse/NeuralNetwork/Network.h
@@ -57,7 +57,7 @@ namespace Impulse {
                         auto prevLayer = this->layers.at(i - 1);
 
                         Matrix tmp1 = layer->W.transpose() * sigma;
-                        Matrix tmp2 = prevLayer->derivative(prevLayer->Z);
+                        Matrix tmp2 = prevLayer->derivative(prevLayer->A);
 
                         sigma = tmp1.array() * tmp2.array();
                     }
